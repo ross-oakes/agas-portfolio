6ed818853efc5614f269d61c48fb000e2f2306b2
ross.oakes <ross.oakes@ocado.com>
2019-12-21 15:21:30 +0000

    Updates

---
+++ precache-manifest.9819b1ca61af2718e3d6442bf576c6dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b4b99c26fd900d0c5d26d3bfbfa499",
+    "url": "/agas-portfolio/index.html"
+  },
+  {
+    "revision": "2ded96f26d3d38ae8bf7",
+    "url": "/agas-portfolio/static/css/2.d9253284.chunk.css"
+  },
+  {
+    "revision": "dd85cf485c3c4886ccde",
+    "url": "/agas-portfolio/static/css/main.bec105e1.chunk.css"
+  },
+  {
+    "revision": "2ded96f26d3d38ae8bf7",
+    "url": "/agas-portfolio/static/js/2.730c3da6.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/agas-portfolio/static/js/2.730c3da6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd85cf485c3c4886ccde",
+    "url": "/agas-portfolio/static/js/main.48034c40.chunk.js"
+  },
+  {
+    "revision": "2c76b946a5253b7500d9",
+    "url": "/agas-portfolio/static/js/runtime-main.9ac5589c.js"
+  },
+  {
+    "revision": "1bf15cfa22c61ea0954ff2dbffe51b17",
+    "url": "/agas-portfolio/static/media/aga-in-louvre.1bf15cfa.png"
+  },
+  {
+    "revision": "1f1365caea39762315e3c7591bb73b35",
+    "url": "/agas-portfolio/static/media/aga-rocks.1f1365ca.jpg"
+  },
+  {
+    "revision": "9b2e1a9c4ec43ba3d15084c7f6490d48",
+    "url": "/agas-portfolio/static/media/gift-giver-cover.9b2e1a9c.png"
+  },
+  {
+    "revision": "cff120ab97eea5b63c2005c3acb9c1ad",
+    "url": "/agas-portfolio/static/media/gift-giver-main-page.cff120ab.png"
+  },
+  {
+    "revision": "1ffa418379cff47451023868cf50dd60",
+    "url": "/agas-portfolio/static/media/gift-giver-sub-page.1ffa4183.png"
+  },
+  {
+    "revision": "ccff23e2246fc7968535a456969f5726",
+    "url": "/agas-portfolio/static/media/paper-prototype.ccff23e2.jpg"
+  },
+  {
+    "revision": "891d790ca27a35f09da4975139b10fcd",
+    "url": "/agas-portfolio/static/media/personas-ben.891d790c.png"
+  },
+  {
+    "revision": "d32baa36e99286d21f4c5a5a5085df1e",
+    "url": "/agas-portfolio/static/media/personas-rachel.d32baa36.png"
+  },
+  {
+    "revision": "e3632efc985e4ce446bd534c8a28d6c4",
+    "url": "/agas-portfolio/static/media/resume.e3632efc.pdf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/agas-portfolio/precache-manifest.9819b1ca61af2718e3d6442bf576c6dd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/agas-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
