098a7ca65de3e53705a2d091e4fac2161ca02059
ross.oakes <ross.oakes@ocado.com>
2019-12-22 12:57:37 +0000

    Fixed about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import AgaLouvre from '../resources/aga-in-louvre.jpg'
+import {ScrollToTop} from "../components/ReactUtils";
 
 const AboutPage = () => {
     return (
         <div className="about-page">
-            <h3 className="about-title">About</h3>
+            <ScrollToTop/>
+            <img className="about-picture" src={AgaLouvre}/>
+            <div className="about-text">
+                <h3 className="about-title">About</h3>
 
-            <p className="about-text">I'm a beginner UX designer with a passion to continuously learn and improve myself and my work.
-            I'm based in St. Albans, UK.</p>
+                <p>I'm a beginner UX designer with a passion to continuously learn and improve myself and my work.
+                    I'm based in St. Albans, UK.</p>
 
-            <p className="about-text">The most important idea of UX for me is the user. I love conducting user research,
-                which allows me to iteratively refine my designs, so that they are functional, clear and pleasing
-                to the eye.</p>
+                <p>Based in St. Albans, UK.</p>
+
+                <p>I love conducting user research and iteratively refining my designs, so that they are functional,
+                    clear and pleasing to the eye.</p>
+
+                <p>I'm a modern art enthusiast.</p>
+
+                <p>Coffee lover.</p>
+
+                <p>I spend my free time travelling and exploring the globe.</p>
+            </div>
+
         </div>
     );
 };
