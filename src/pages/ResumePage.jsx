9a46b7cb88dc6d6e0f771a19e4d3ef87d1782384
ross.oakes <ross.oakes@ocado.com>
2019-12-22 09:12:22 +0000

    Fixed Resume Page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import icon from '../resources/download-icon.png';
+import downloadIcon from '../resources/download-icon.png';
+import linkedinIcon from '../resources/linkedIn-icon.png';
 import resume from '../resources/resume.pdf';
 
 const ResumePage = () => {
     return (
         <div className="resume-page">
-            <h3 className="resume-title"><b>Education</b></h3>
-            <p><b>Masters, </b>English Philology, Jagiellonian University, 2015-2017, Grade: Distinction.</p>
+            <div>
+                <h3 className="resume-title"><b>Education</b></h3>
+                <p><b>Masters, </b>English Philology, Jagiellonian University, 2015-2017, Grade: Distinction.</p>
 
-            <h3 className="resume-title"><b>Training</b></h3>
-            <p>Travis, David. <b>User Experience (UX): The Ultimate Guide to Usability and UX.</b> Udemy, 2019.</p>
-            <p>Walter Scott, Daniel. <b>User Experience Design Essentials - Adobe XD UI UX Design.</b> Udemy, 2019.</p>
-            <p>Natoli, Joe. <b>DESIGN RULES: Principles + Practices for Great UI Design.</b> Udemy, 2019.</p>
+                <h3 className="resume-title"><b>Training</b></h3>
+                <p>Travis, David. <b>User Experience (UX): The Ultimate Guide to Usability and UX.</b> Udemy, 2019.</p>
+                <p>Walter Scott, Daniel. <b>User Experience Design Essentials - Adobe XD UI UX Design.</b> Udemy, 2019.</p>
+                <p>Natoli, Joe. <b>DESIGN RULES: Principles + Practices for Great UI Design.</b> Udemy, 2019.</p>
+                <p>Barnard, Leon. <b>Wireframing with Balsamiq Mockups.</b> Udemy, 2019.</p>
+                <p>Natoli, Joe. <b>UX & Web Design Master Course: Strategy, Design, Development.</b> Udemy, 2019.</p>
+            </div>
+            <div className="resume-footer">
+                <a href={resume} download>
+                    <img src={downloadIcon} alt="" height="25px" width="25px"/> Download My Resume
+                </a>
+                <a href="https://www.linkedin.com/in/agnieszka-matyja-09b07b198/">
+                    <img src={linkedinIcon} alt="" height="25px" width="25px"/> LinkedIn profile
+                </a>
+            </div>
 
-            <a className="resume-footer" href={resume} download>
-                <img src={icon} alt="My Resume" height="25px" width="25px"/> My Resume
-            </a>
         </div>
     );
 };
