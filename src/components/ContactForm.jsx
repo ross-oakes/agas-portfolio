21977c91ffe27e621345358d1b4ad78222d4f0a3
ross.oakes <ross.oakes@ocado.com>
2019-12-27 11:38:37 +0000

    Got sending email working, also added google analytics

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,36 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Form} from 'reactstrap';
 import humanIcon from "../resources/human-icon.png"
 import mailIcon from "../resources/mail-icon.png"
 import ContactFormBox from "./ContactFormBox";
 
-const ContactForm = () =>  (
-    <Form className="form">
+const ContactForm = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [content, setContent] = useState();
+
+    const send = () => {
+        var template_params = {
+            "reply_to": email,
+            "from_name": name,
+            "message_html": content
+        };
+
+        var service_id = "default_service";
+        var template_id = "template_594ZU3dk";
+        window.emailjs.send(service_id, template_id, template_params)
+    };
+
+    function wrapFunc(func){
+        return function(event){
+            return func(event.target.value);
+        }
+    }
+
+    return <Form className="form">
         <tr>
             <ContactFormBox icon={humanIcon}
                             label="Name"
                             hintText="Jane Doe"
-                            height="40"/>
+                            height="40"
+                            onChange={wrapFunc(setName)}/>
         </tr>
         <tr>
             <ContactFormBox icon={mailIcon}
                             label="Email"
                             hintText="janedoe123@email.com"
-                            height="40"/>
+                            height="40"
+                            onChange={wrapFunc(setEmail)}/>
         </tr>
         <tr>
             <ContactFormBox label="Message"
                             hintText="Type something..."
                             inputType="textarea"
-                            height="180"/>
+                            height="180"
+                            onChange={wrapFunc(setContent)}/>
         </tr>
         <tr align="right">
-            <Button className="button-display">
+            <Button className="button-display" onClick={send}>
                 <b>SEND</b>
             </Button>
         </tr>
 
     </Form>
-);
+};
 
 export default ContactForm;
--- src/components/ContactFormBox.jsx
+++ src/components/ContactFormBox.jsx
@@ -1,31 +1,32 @@
 import * as React from "react";
 import {FormGroup, Input, Label} from "reactstrap";
 
-const ContactFormBox = ({icon, inputType, label, hintText, height}) => (
-        <FormGroup>
-            <table>
-                <tbody>
-                <tr className="form-group-display">
-                    <td className="icon-display">
-                        {icon &&
-                        <img className="icon-size" src={icon} alt=""/>}
-                    </td>
-                    <td className="td-custom">
-                        <div>
-                            <Label className="label-display">{label}</Label>
-                            <Input className="input-display"
-                                   type={inputType || "text"}
-                                   name="user name"
-                                   id="username"
-                                   placeholder={hintText}
-                                   style={{height: height + 'px'}}
+const ContactFormBox = ({icon, inputType, label, hintText, height, onChange}) => (
+    <FormGroup>
+        <table>
+            <tbody>
+            <tr className="form-group-display">
+                <td className="icon-display">
+                    {icon &&
+                    <img className="icon-size" src={icon} alt=""/>}
+                </td>
+                <td className="td-custom">
+                    <div>
+                        <Label className="label-display">{label}</Label>
+                        <Input className="input-display"
+                               type={inputType || "text"}
+                               name="user name"
+                               id="username"
+                               placeholder={hintText}
+                               style={{height: height + 'px'}}
+                               onChange={onChange}
                         />
-                        </div>
-                    </td>
-                    </tr>
-                </tbody>
-            </table>
-        </FormGroup>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </FormGroup>
 );
 
 export default ContactFormBox;
