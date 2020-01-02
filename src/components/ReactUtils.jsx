9f4d3b1ea42ac5dc6eb0a7c1f89062877e5fe53d
ross.oakes <ross.oakes@ocado.com>
2020-01-02 13:10:42 +0000

    fixed mounting properly so screen actually rolls to top

--- src/components/ReactUtils.jsx
+++ src/components/ReactUtils.jsx
@@ -5,6 +5,10 @@ export class ScrollToTop extends Component {
         window.scrollTo(0, 0)
     }
 
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
+
     render() {
         return null;
     }
