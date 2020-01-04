2bbdd99d5ef2288110b11d3487468d49d940c903
ross.oakes <ross.oakes@ocado.com>
2020-01-04 17:00:29 +0000

    Fixed navbar so it closes when choosing

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -20,13 +20,14 @@ const NavBar = () => {
     const [isDropdownOpen, setDropdownOpen] = useState(false);
 
     const toggleNavBar = () => setIsOpen(!isOpen);
+    const closeNavBar = () => setIsOpen(false);
     const toggleDropDown = () => setDropdownOpen(!isDropdownOpen);
 
     return (
         <Navbar className="navbar" expand="md" light fixed="top">
             <NavbarBrand className="navbar-name" tag={Link} to="/">aga ux</NavbarBrand>
-            <NavbarToggler onClick={toggleNavBar} />
-            <Collapse isOpen={false} navbar>
+            <NavbarToggler onClick={toggleNavBar}/>
+            <Collapse isOpen={isOpen} navbar>
                 <Nav navbar>
                     <Dropdown nav inNavbar
                               isOpen={isDropdownOpen}
@@ -34,27 +35,27 @@ const NavBar = () => {
                         <DropdownToggle nav caret>
                             Portfolio
                         </DropdownToggle>
-                        <DropdownMenu right>
+                        <DropdownMenu right className="sub-navbar">
                             <DropdownItem>
-                                <NavLink tag={HashLink} to="/#my-portfolio">Overview</NavLink>
+                                <NavLink tag={HashLink} to="/#my-portfolio" onClick={closeNavBar}>Overview</NavLink>
                             </DropdownItem>
                             <DropdownItem divider/>
                             <DropdownItem>
-                                <NavLink tag={Link} to="/gift-giver">Gift Giver</NavLink>
+                                <NavLink tag={Link} to="/gift-giver" onClick={closeNavBar}>Gift Giver</NavLink>
                             </DropdownItem>
                             <DropdownItem>
-                                <NavLink tag={Link} to="/ui-challenges">UI Challenges</NavLink>
+                                <NavLink tag={Link} to="/ui-challenges" onClick={closeNavBar}>UI Challenges</NavLink>
                             </DropdownItem>
                         </DropdownMenu>
                     </Dropdown>
                     <NavItem>
-                        <NavLink tag={Link} to="/about">About</NavLink>
+                        <NavLink tag={Link} to="/about" onClick={closeNavBar}>About</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink tag={Link} to="/resume/">Resume</NavLink>
+                        <NavLink tag={Link} to="/resume/" onClick={closeNavBar}>Resume</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink tag={Link} to="/contact/">Contact</NavLink>
+                        <NavLink tag={Link} to="/contact/" onClick={closeNavBar}>Contact</NavLink>
                     </NavItem>
                 </Nav>
             </Collapse>
