ff9d4acf2e67d395864958e9cf58fdc9aa2eca48
ross.oakes <ross.oakes@ocado.com>
2020-01-04 17:46:09 +0000

    added another course

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -20,6 +20,7 @@ const ResumePage = () => {
                 <p>Barnard, Leon. <b>Wireframing with Balsamiq Mockups.</b> 2019.</p>
                 <p>Natoli, Joe. <b>UX & Web Design Master Course: Strategy, Design, Development.</b> 2019.</p>
                 <p>Angelo Todaro, Joseph. <b>Sketch from A to Z (2019): Become an app designer.</b> 2020.</p>
+                <p>Travis, David. <b>Usability Testing Boot Camp.</b> 2020.</p>
             </div>
             <Container className="resume-footer">
                 <Row>
