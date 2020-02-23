70cf98b916a1ab46d49e824da9026bb1236a31ba
ross.oakes <ross.oakes@ocado.com>
2020-02-23 14:19:46 +0000

    added ielts essays brief section

--- src/portfolio-pages/IELTSEssays.jsx
+++ src/portfolio-pages/IELTSEssays.jsx
@@ -3,12 +3,24 @@ import PortfolioPage from "./components/PortfolioPage";
 import ProjectTitle from "./components/ProjectTitle";
 
 import IELTSEssaysCover from "../resources/ieltsessays/cover.png"
+import ProjectSubsection from "./components/ProjectSubsection";
 
 const IELTSEssays = () => (
     <PortfolioPage>
         <ProjectTitle title="IELTS Essays"
                       subtitle="an app to help students prepare for IELTS English exam"
                       picture={IELTSEssaysCover}/>
+
+        <ProjectSubsection title="The brief">
+            <p>
+                The brief was to redesign an existing app - IELTS Essays. This app's purpose is to help students learn
+                writing essays for IELTS exam. It has a collection of essays, together with feedback on them, to help
+                students understand how to improve their writing skills. Other functions of this app are to give
+                students some information about the speaking part of the exam and marking criteria. The last important
+                feature was to present students with a list of vocabulary, so that they can use it when they write
+                their own essays.
+            </p>
+        </ProjectSubsection>
     </PortfolioPage>
 );
 
