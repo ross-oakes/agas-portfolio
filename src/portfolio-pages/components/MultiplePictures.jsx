d6f3a7136aab48ffed1d8f07508ea80e0866887c
ross.oakes <ross.oakes@ocado.com>
2020-02-26 19:11:08 +0000

    fixed multiple pictures using proper spacing

--- src/portfolio-pages/components/MultiplePictures.jsx
+++ src/portfolio-pages/components/MultiplePictures.jsx
@@ -10,10 +10,8 @@ const createPictures = (picturesObject, horizontal) => {
     const spaceSize = 1;
     const pictureSize = (12 - (numberOfSpacesPerLine * spaceSize)) / numberOfPicturesPerLine;
 
-    const spacer = <Col md={spaceSize}></Col>;
-
-    const htmlPics = picturesObject.map(function (pictureObject) {
-        return <Col key={pictureObject} md={pictureSize}>
+    return picturesObject.map(function (pictureObject) {
+        return <Col key={pictureObject} md={pictureSize} className="multi-picture-spacing">
             <div className="picture-with-description">
                 <img className="image-shading" src={pictureObject.src} alt={pictureObject.description}
                      style={{
@@ -24,8 +22,6 @@ const createPictures = (picturesObject, horizontal) => {
             </div>
         </Col>
     });
-
-    return [...htmlPics].map((e, i) => i < htmlPics.length - 1 ? [e, spacer] : [e]).reduce((a, b) => a.concat(b));
 };
 
 const MultiplePictures = ({pictures: picturesObject, horizontal}) => (
