9bcd1055a98f0ac54196f7169689c1cebf452868
ross.oakes <ross.oakes@ocado.com>
2020-03-02 19:39:21 +0000

    updated text in about me page

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -10,20 +10,33 @@ const AboutMePage = () => {
             <div className="about-me-text">
                 <h3 className="about-me-title">About Me</h3>
 
-                <p>I am a UX designer with a passion to learn and improve myself and my work.</p>
-
-                <p>Based in St. Albans, UK.</p>
-
-                <p>I love conducting user research, as it provides me guidance when refining my designs. This means
-                    they become more functional, clear and pleasing to the eye.</p>
-
-                <p>I'm a modern art enthusiast.</p>
-
-                <p>Sports: skiing, pilates, gym.</p>
-
-                <p>Coffee lover.</p>
-
-                <p>I spend my free time travelling and exploring the globe.</p>
+                <p>
+                    I am a UX designer with a passion to constantly learn and improve myself and my work.
+                </p>
+                <p>
+                    I love conducting user research, as it provides me with guidance when refining my designs. I believe
+                    that there is no UI without UX research.
+                </p>
+                <p>
+                    I spend my free time learning more about UX. Whether by reading books or completing online courses.
+                    I am especially interested in how psychology provides insights into user research. I also like
+                    learning about universal design principles.
+                </p>
+                <p>
+                    I love to innovate. I make sure I am up-to-date with latest technological advancements. At the
+                    moment, I am a member of The Interaction Design Foundation, a UX community, which offers UX courses
+                    for professionals. I’m taking part in a Virtual Reality and Augmented Reality course as I believe
+                    these skills are so important for the future!
+                </p>
+                <p>
+                    I'm a modern art enthusiast. My creativity originated with exploring the unknown and extending the
+                    boundaries of what was traditionally thought of as art. My favourite painter is Mark Rothko.
+                </p>
+                <p>
+                    As maximum productivity needs mental rest, I do Pilates, go skiing, travel the world and keep an
+                    open-minded attitude, giving me the chance to absorb new knowledge. Wherever I go I keep my brain in
+                    an enquiry mode - I speak to people, work with people and learn from those around me.
+                </p>
             </div>
         </PortfolioPage>
     );
