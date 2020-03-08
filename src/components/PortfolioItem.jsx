ace75a91a0ba2eedfdb1ffb61420f3f465c4464a
ross.oakes <ross.oakes@ocado.com>
2020-03-08 18:23:36 +0000

    improved portfolio item text for mobile

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
 
-const PortfolioItem = ({ title, image, details, endpoint}) => (
+const PortfolioItem = ({ title, image, details, endpoint, textColour}) => (
      <div className="portfolio-cover">
          <Link to={endpoint}>
             <img alt={title} src={image} className="portfolio-item"/>
             <div className="hovered-filter">
-                <div className="portfolio-text">
+                <div className="portfolio-text" style={{color: textColour}}>
                     <b>{title}</b>
                     <p className="portfolio-details">{details}</p>
                 </div>
