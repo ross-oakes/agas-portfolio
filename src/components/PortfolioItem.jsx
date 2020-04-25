6e8ced1e1dc8a459ee2606be6b6db80887cd41ff
ross.oakes <ross.oakes@ocado.com>
2020-04-25 17:01:28 +0100

    made titles in portolio items have a header in playfair instead of roboto

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
 
-const PortfolioItem = ({ title, image, details, endpoint, textColour}) => (
+const PortfolioItem = ({ title, image, details, endpoint}) => (
      <div className="portfolio-cover">
          <Link to={endpoint}>
             <img alt={title} src={image} className="portfolio-item"/>
             <div className="hovered-filter">
-                <div className="portfolio-text" style={{color: textColour}}>
-                    <b>{title}</b>
+                <div className="portfolio-text">
+                    <div className="portfolio-item-title">{title}</div>
                     <p className="portfolio-details">{details}</p>
                 </div>
             </div>
